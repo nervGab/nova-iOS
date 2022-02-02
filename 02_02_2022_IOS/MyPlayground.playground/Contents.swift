d2db06e2413f00ad33071f92a9c6c3874af78003
Juan Manuel Pereira Sanchez <jmps1389@gmail.com>
2022-02-02 01:01:10 +0100

    [WIP] - my first commit -

---
+++ 02_02_2022_IOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// -> una línea
+
+/*
+ ->  varias líneas
+ */
+
+// MARK: - TIPOS BASICOS
+
+var nombre: String = "pepe" // var -> define una propiedad mutable
+let apellido: String = "ramirez"
+
