425d849450710963da048f670b11fb605208b713
Juan Manuel Pereira Sanchez <pereiras.juanma@gmail.com>
2022-02-02 18:08:26 +0100

    [WIP] - added labels, buttons & constraints -

--- 02_02_2022_IOS/AppHelloWorld/AppHelloWorld/ViewController.swift
+++ 02_02_2022_IOS/AppHelloWorld/AppHelloWorld/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - IBOutlet
+
+    @IBOutlet weak var firstLBLTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var helloWorldLBL: UILabel!
+    @IBOutlet weak var byeWorldLBL: UILabel!
+    // MARK: - IBActions
+    @IBAction func changeNameAction(_ sender: Any) {
+        helloWorldLBL.text = "hello NOVA iOS emeal NTTDATA"
+        byeWorldLBL.text = "bye bye NTTDATA!"
+    }
+    // MARK: - life VC
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        print("hello NOVA")
+        firstLBLTrailingConstraint.constant = 50
     }
 
-
+    
 }
 
+// MARK: - Extension VC
