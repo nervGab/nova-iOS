83cd5958e6e978a2ea2fef84628530903d81891a
Juan Manuel Pereira Sanchez <pereiras.juanma@gmail.com>
2022-02-05 16:38:56 +0100

    [WIP] - added pickerView in secondVC, dataSource y delegate code extension -
    + added other resources in asset catalog

--- 03_02_2022_IOS/DayPicker/DayPicker/Modules/Sections/SecondViewController.swift
+++ 03_02_2022_IOS/DayPicker/DayPicker/Modules/Sections/SecondViewController.swift
@@ -13,16 +13,63 @@ class SecondViewController: UIViewController {
     var pickerCheeseArrayData = ["mozzarella", "Gorgonzola", "Provolone", "Stilton", "Asiago"]
     
     // MARK: - IBOutlets
-    @IBOutlet weak var titleChesseLBL: UILabel!
-    @IBOutlet weak var detailChesseTexView: UITextView!
+    @IBOutlet weak var titleCheeseLBL: UILabel!
+    @IBOutlet weak var detailCheeseTextView: UITextView!
     @IBOutlet weak var chesseImageView: UIImageView!
-    @IBOutlet weak var selectionChessePickerView: UIPickerView!
+    @IBOutlet weak var selectionCheesePickerView: UIPickerView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.selectionCheesePickerView.delegate = self
+        self.selectionCheesePickerView.dataSource = self
+        
+        self.title = pickerCheeseArrayData[0]
+        self.titleCheeseLBL.text = pickerCheeseArrayData[0]
+        self.chesseImageView.image = UIImage(named: pickerCheeseArrayData[0])
+        self.detailCheeseTextView.text = "plastic or stretched-curd cheese; the curd is mixed with heated whey and stretched and kneaded until it attains a smooth, pliable consistency. ... It is then molded into spheres or ovals and stored in water to keep it moist."
     }
 
+}
+
+extension SecondViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerCheeseArrayData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 40
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.title = pickerCheeseArrayData[row]
+        self.titleCheeseLBL.text = pickerCheeseArrayData[row]
+        self.chesseImageView.image = UIImage(named: pickerCheeseArrayData[row])
+        switch row {
+        case 0:
+            self.detailCheeseTextView.text = "pliable consistency. ... It is then molded into spheres or ovals and stored in water to keep it moist."
+        case 1:
+            self.detailCheeseTextView.text = "the curd is mixed with heated whey and stretched and kneaded until it attains a smooth, pliable consistency."
+        case 2:
+            self.detailCheeseTextView.text = "the curd is mixed with heated whey and stretched and kneaded until it attains a smooth."
+        case 3:
+            self.detailCheeseTextView.text = "the curd is mixed with heated whey and stretched and kneaded."
+        case 4:
+            self.detailCheeseTextView.text = "the curd is mixed with heated"
+        default:
+            self.detailCheeseTextView.text = "the curd is "
+        }
+    }
+}
+
+extension SecondViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerCheeseArrayData.count
+    }
 }
