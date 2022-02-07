0e8f9298c09c3942defb6c348634c7791f615973
Juan Manuel Pereira Sanchez <pereiras.juanma@gmail.com>
2022-02-08 00:09:54 +0100

    [WIP] - AlertsApp 1st commit -
    + added 1st screen buttons
    + added 2nd and 3rd CV

---
+++ 07_02_2022/AppAlerts/AppAlerts/Modules/Sections/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  AppAlerts
+//
+//  Created by Juan Manuel Pereira Sanchez on 7/2/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 07_02_2022/AppAlerts/AppAlerts/Modules/Sections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppAlerts
+//
+//  Created by Juan Manuel Pereira Sanchez on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
