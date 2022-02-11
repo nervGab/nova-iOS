0711c99f81c9a2c56d67d3802f0d7b9fd7557db5
Juan Manuel Pereira Sanchez <pereiras.juanma@gmail.com>
2022-02-11 22:06:44 +0100

    [WIP] - AppIntroTableView first commit -

---
+++ 08_02_2022/AppIntroTableView/AppIntroTableView/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AppIntroTableView
+//
+//  Created by Juan Manuel Pereira Sanchez on 11/2/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
