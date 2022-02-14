2ae3a5750b0c3acd20473583df786b75192bb7bb
Juan Manuel Pereira Sanchez <pereiras.juanma@gmail.com>
2022-02-14 01:29:09 +0100

    [WIP] - Added oulets, actions and prepare() in 2nd and 3rd VC -

--- 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/SecondViewController.swift
+++ 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/SecondViewController.swift
@@ -10,22 +10,81 @@ import UIKit
 class SecondViewController: UIViewController {
     // MARK - Global variables
     var variables = GlobalUserData()
+    var dogAgeInt: Int?
+    // MARK - Outlets
+    @IBOutlet weak var nameLBL: UILabel!
+    @IBOutlet weak var surnameLBL: UILabel!
+    @IBOutlet weak var dogAgeLBL: UILabel!
+    
+    @IBOutlet weak var dogAgeTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var addressTF: UITextField!
+    
+    
+
+    
+    // MARK: - Actions
+    @IBAction func DogAgeTFAction(_ sender: Any) {
+        dogAgeInt = Int(dogAgeTF.text ?? "")
+        if let DogAgeDes = dogAgeInt {
+            let realDogAge = DogAgeDes * 7
+            self.dogAgeLBL.text = "the age of my dog is: \(realDogAge)"
+        } else {
+            present(displayAlertVC(title: "Error",
+                                   message: "please. set your dog age"),
+                    animated: true) {
+                //we pop to the previous view, after alert completion, with navigationController
+//                if let nav = self.navigationController {
+//                    nav.popViewController(animated: true)
+//                }
+                self.popVC()
+            }
+        }
+    }
+    
+    func popVC() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        let navController = UINavigationController(rootViewController: vc)
+        navController.popViewController(animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
         // Do any additional setup after loading the view.
     }
     
+    private func setupUI(){
+        self.nameLBL.text = variables.nameData
+        self.surnameLBL.text = variables.surnameData
+        self.phoneTF.keyboardType = .numberPad
+        self.dogAgeTF.keyboardType = .numberPad
+    }
 
-    /*
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        if segue.identifier == "segueV3" {
+            if !(self.dogAgeTF.text?.isEmpty ?? false) && !(self.phoneTF.text?.isEmpty ?? false) && !(self.addressTF.text?.isEmpty ?? false) {
+                let vc3 = segue.destination as! ThirdViewController
 
+                vc3.variables = variables
+                vc3.variables.dogAgeData = self.dogAgeLBL.text
+                vc3.variables.phoneData = self.phoneTF.text
+                vc3.variables.addressData = self.addressTF.text
+
+            } else {
+                present(displayAlertVC(title: "Hey!",
+                                       message: "you have to fill the form"),
+                        animated: true,
+                        completion: nil)
+            }
+        }
+    }
 }
--- 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/ThirdViewController.swift
+++ 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/ThirdViewController.swift
@@ -8,22 +8,67 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    // MARK - Global variables
+    var variables = GlobalUserData()
+    
+    // MARK - Oulets
 
+    @IBOutlet weak var nameLBL: UILabel!
+    @IBOutlet weak var surnameLBL: UILabel!
+    @IBOutlet weak var phoneLBL: UILabel!
+    @IBOutlet weak var addressLBL: UILabel!
+    @IBOutlet weak var dogAgeLBL: UILabel!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var postalCodeTF: UITextField!
+    @IBOutlet weak var cityTF: UITextField!
+    
+    // MARK - Actions
+    @IBAction func nextScreen(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
         // Do any additional setup after loading the view.
     }
     
+    private func setupUI(){
+        self.nameLBL.text = variables.nameData
+        self.surnameLBL.text = variables.surnameData
+        self.phoneLBL.text = variables.phoneData
+        self.addressLBL.text = variables.addressData
+        self.dogAgeLBL.text = variables.dogAgeData
 
-    /*
+        self.phoneTF.keyboardType = .numberPad
+        self.postalCodeTF.keyboardType = .numberPad
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+  
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "segueV4" {
+            if !(self.phoneTF.text?.isEmpty ?? false) && !(self.postalCodeTF.text?.isEmpty ?? false) && !(self.cityTF.text?.isEmpty ?? false) {
+                let vc4 = segue.destination as! ThirdViewController
+
+                vc4.variables = variables
+                vc4.variables.geoPositionData = self.phoneTF.text
+                vc4.variables.postalCodeData = self.postalCodeTF.text
+                vc4.variables.cityData = self.cityTF.text
+
+            } else {
+                present(displayAlertVC(title: "Hey!",
+                                       message: "you have to fill the form"),
+                        animated: true,
+                        completion: nil)
+            }
+        }
     }
-    */
 
 }
