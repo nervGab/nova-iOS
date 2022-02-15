89d099e8165dd631edf32828eccb1a5c09c2672f
Juan Manuel Pereira Sanchez <jmps1389@gmail.com>
2022-02-15 17:36:29 +0100

    [WIP] - added picker for geoLocation in thirdVC -

--- 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/ThirdViewController.swift
+++ 09_02_2022/AppNavigation/AppNavigation/Modules/Sections/ThirdViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class ThirdViewController: UIViewController {
     // MARK - Global variables
     var variables = GlobalUserData()
-    
+    var cityLocationDataSource = ["Valencia", "Madrid", "barcelona", "Vigo"]
+    var postalCodeDataSource = ["28010", "28009", "28017", "28000"]
+    var geoLocationDataSource = ["140.4512/-34.2514", "98.3452/-53.2784", "432.5678/-34.3434", "140.4512/-34.2514"]
     // MARK - Oulets
 
     @IBOutlet weak var nameLBL: UILabel!
@@ -18,14 +20,16 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var phoneLBL: UILabel!
     @IBOutlet weak var addressLBL: UILabel!
     @IBOutlet weak var dogAgeLBL: UILabel!
-    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!//location
     @IBOutlet weak var postalCodeTF: UITextField!
-    @IBOutlet weak var cityTF: UITextField!
+    @IBOutlet weak var cityTF: UITextField!//geolocation
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         // Do any additional setup after loading the view.
+        
     }
     
     private func setupUI(){
@@ -35,8 +39,9 @@ class ThirdViewController: UIViewController {
         self.addressLBL.text = variables.addressData
         self.dogAgeLBL.text = variables.dogAgeData
 
-        self.phoneTF.keyboardType = .numberPad
-        self.postalCodeTF.keyboardType = .numberPad
+       self.phoneTF.inputView = createPickerView(textField: phoneTF, array: cityLocationDataSource, tag: 0, delegate: self, dataSourcePicker: self)
+        self.postalCodeTF.inputView = createPickerView(textField: postalCodeTF, array: postalCodeDataSource, tag: 1, delegate: self, dataSourcePicker: self)
+        self.cityTF.inputView = createPickerView(textField: cityTF, array: geoLocationDataSource, tag: 2, delegate: self, dataSourcePicker: self)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,4 +71,49 @@ class ThirdViewController: UIViewController {
         }
     }
 
+}
+
+extension ThirdViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 0:
+            return cityLocationDataSource[row]
+        case 1:
+            return postalCodeDataSource[row]
+        default:
+            return geoLocationDataSource[row]
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 0:
+            phoneTF.text =  cityLocationDataSource[row]
+            self.view.endEditing(true)
+
+        case 1:
+            postalCodeTF.text =  postalCodeDataSource[row]
+            self.view.endEditing(true)
+
+        default:
+            cityTF.text = geoLocationDataSource[row]
+            self.view.endEditing(true)
+        }
+//        self.view.endEditing(true)
+    }
+}
+extension ThirdViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 0:
+            return cityLocationDataSource.count
+        case 1:
+            return postalCodeDataSource.count
+        default:
+            return geoLocationDataSource.count
+        }
+    }
 }
