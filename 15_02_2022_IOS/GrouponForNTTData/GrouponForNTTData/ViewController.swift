c619f3ff73c79d1270bae487b4a6efeaa341cf4b
Juan Manuel Pereira Sanchez <jmps1389@gmail.com>
2022-03-05 23:43:39 +0100

    [WIP] - groupon 1st commit -

---
+++ 15_02_2022_IOS/GrouponForNTTData/GrouponForNTTData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrouponForNTTData
+//
+//  Created by Juan Manuel Pereira Sanchez on 5/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
